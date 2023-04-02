ffb364fd0f717bbac44b8ad518e51980e38a1b61
khaled492007 <khmo492007@gmail.com>
2023-04-02 03:59:14 +0200

    add static files and conection

--- home/views.py
+++ home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index (request):
-    return HttpResponse("HOME PAGE")
+    #return HttpResponse("HOME PAGE")
+    return render(request,'home/index.html')
