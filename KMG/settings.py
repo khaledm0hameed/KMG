c2cc86f23fe0fb92f78a1e257f2c8a662c49527a
unknown <khmo492007@gmail.com>
2023-05-19 00:16:39 +0300

    add db in project

--- KMG/settings.py
+++ KMG/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+ 
 import os
 from pathlib import Path
 
@@ -81,14 +82,19 @@ WSGI_APPLICATION = 'KMG.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'KMG',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5000',
     }
 }
 
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
