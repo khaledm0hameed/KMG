72c39e27fbe8774390b10125a8fa447251b7541c
khaledm0hameed <khmo492007@gmail.com>
2023-05-19 22:47:54 +0300

    run project

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    name=models.CharField(max_length=max)
+    name=models.CharField(max_length=300)
     price=models.DecimalField(max_digits=50,decimal_places=3)
     imge=models.ImageField(upload_to='photos/%y/%m/%d')
     description=models.TextField()
