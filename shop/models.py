e6d5664f3934454c17a6f6fcf999aca9d06fa323
khaled492007 <khmo492007@gmail.com>
2023-05-19 22:37:26 +0300

    add models and admin panel

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=max)
+    price=models.DecimalField(max_digits=50,decimal_places=3)
+    imge=models.ImageField(upload_to='photos/%y/%m/%d')
+    description=models.TextField()
+
