5650cec2decfb32f83c53ff26bdca203099b7dc1
khaledm0hameed <khmo492007@gmail.com>
2023-05-21 23:38:41 +0300

    add media photos

--- KMG/urls.py
+++ KMG/urls.py
@@ -24,6 +24,8 @@ from payment import urls
 from django.contrib import admin
 from django.urls import path, include
 from contact import urls
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
@@ -36,4 +38,4 @@ urlpatterns = [
     #path('orderdetail/',include('orderdetail.urls')),
     #path('payment',include('payment.urls')),
     
-]
+] + static(settings.MEDIA_URL,document_ROOT=settings.MEDIA_ROOT)
