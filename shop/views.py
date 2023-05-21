6061d754e8914172ba526a287931afb933755899
khaledm0hameed <khmo492007@gmail.com>
2023-05-21 16:56:52 +0300

    get product from db

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 # Create your views here.
 
 def shop(request):
-    return render(request,'shop/shop.html')
+    return render(request,'shop/shop.html',{'pro':Product.objects.all()})
