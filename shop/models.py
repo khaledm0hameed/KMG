ee959b293b31ae25c03c8e7aa7f346dfaef9bae9
khaledm0hameed <khmo492007@gmail.com>
2023-05-24 17:15:32 +0300

    add category and new product

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 class Product(models.Model):
+    choise=[
+        ('Laptop','Laptop'),
+        ('Computer','Computer'),
+        ('Phone','Phone'),
+        ('Screen','Screen'),
+    ]
     name=models.CharField(max_length=300)
     price=models.DecimalField(max_digits=50,decimal_places=3)
-    imge=models.ImageField(upload_to='photos/%y/%m/%d')
+    imge=models.ImageField(upload_to='photos/%y/%m/%d',default='photos/unknow.jpg')
     description=models.TextField()
+    category=models.CharField(max_length=100,null=True,choices=choise)
 
+    def __str__(self,):
+        return self.name
+    
+
+    class Meta:
+        ordering = ['price']
