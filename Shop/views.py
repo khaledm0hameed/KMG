4f85fdb0eb2a425de6bbb67d7ef8cf0574af9528
admin <khmo492007@gmail.com>
2023-06-01 23:23:43 +0300

    add sort buy price

--- Shop/views.py
+++ Shop/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import get_object_or_404
 from .models import Product
 # Create your views here.
 def productes(request):
-    return render(request, 'shop/shop.html',{'Pro':Product.objects.all()})
+    Pro=Product.objects.order_by('price') 
+    return render(request, 'shop/shop.html',{'Pro':Pro})
 
 
 
 def product_detail(request,id):
     obj=get_object_or_404(Product,pk=id)
     return render(request,'shop/detail.html',{'obj':obj})
+
+
+
+
